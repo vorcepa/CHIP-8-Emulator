a85a2884eaeb3040f19f9c1c7460c9c3fe44f4af
Phillip Vorce <vorcepa@gmail.com>
2018-07-27 17:44:59 -0600

    Initial commit; renders window, exit button and exit key added

---
+++ emulator.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[]){
+	SDL_Window *window = NULL;
+	SDL_Surface *surface = NULL;
+
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Event userInput;
+
+	int GameLoop = 1;
+
+
+	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0){
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return 1;
+	}
+
+	window = SDL_CreateWindow(
+		"An SDL Window",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		640,
+		480,
+		SDL_WINDOW_OPENGL
+	);
+
+	if (window == NULL){
+		printf("Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+	else{
+		surface = SDL_GetWindowSurface(window);
+	}
+
+	while(GameLoop){
+		SDL_UpdateWindowSurface(window);
+		while(SDL_PollEvent(&userInput)){
+			if(userInput.type == SDL_KEYUP){
+				switch(userInput.key.keysym.sym){
+					case SDLK_q:
+						GameLoop = 0;
+						break;
+					default:
+						break;
+				}
+			}
+			if(userInput.type == SDL_QUIT){
+				GameLoop = 0;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+}
